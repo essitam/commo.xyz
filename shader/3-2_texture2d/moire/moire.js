a1628b1227bf4fc804bef3192d83171cf433ced9
essitam <matisseam@hotmail.com>
2021-01-30 17:06:59 -0500

    woot

--- shader/3-2_texture2d/moire/moire.js
+++ shader/3-2_texture2d/moire/moire.js
@@ -19,14 +19,20 @@ function setup() {
 function draw() {
   clear();
 
+  // cullShader.setUniform('uSampler', source);
+  // // cullShader.setUniform('mouse', [mouseX/width, mouseY/height]);
+  // cullShader.setUniform('time', millis()/1000);
+  // cullShader.setUniform('amplitude', 0.05 + Math.max(0, 0.03*Math.cos(0.001*millis())));
+  // cullShader.setUniform('amplitudeY', Math.max(0, 0.02*Math.sin(0.001*millis())));
+  // cullShader.setUniform('frequency', 10 + 2 * Math.sin(0.0007*millis()));
+  // cullShader.setUniform('speed', 2.0);
   cullShader.setUniform('uSampler', source);
   // cullShader.setUniform('mouse', [mouseX/width, mouseY/height]);
   cullShader.setUniform('time', millis()/1000);
-  cullShader.setUniform('amplitude', 0.05 + Math.max(0, 0.03*Math.cos(0.001*millis())));
-  cullShader.setUniform('amplitudeY', 0.05 + Math.max(0, 0.03*Math.cos(0.001*millis())));
+  cullShader.setUniform('amplitude', 0.02 + Math.max(0, 0.0001*Math.sin(0.001*millis())));
+  cullShader.setUniform('amplitudeY', Math.max(0, 0.0001*Math.sin(0.001*millis())));
   cullShader.setUniform('frequency', 10 + 2 * Math.sin(0.0007*millis()));
-  cullShader.setUniform('speed', 2.0);
-
+  cullShader.setUniform('speed', 1.0);
 
   shader(cullShader);
 
